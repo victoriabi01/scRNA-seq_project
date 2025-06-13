915b0c2f4b10d4230f066be54015435c038d3762
victoriabi01 <victoriabi22808@gmail.com>
2025-06-12 20:21:52 -0400

    preprocessing scanpy

--- learning/preprocessing.py
+++ learning/preprocessing.py
@@ -5,4 +5,25 @@ import anndata as ad
 # Data retrieval
 import pooch
 
-# sc.settings.set_figure_params(dpi=50, facecolor="white")
+sc.settings.set_figure_params(dpi=5, facecolor="white")
+EXAMPLE_DATA = pooch.create(
+    path=pooch.os_cache("scverse_tutorials"),
+    base_url="doi:10.6084/m9.figshare.22716739.v1/",
+)
+EXAMPLE_DATA.load_registry_from_doi()
+samples = {
+    "s1d1": "s1d1_filtered_feature_bc_matrix.h5",
+    "s1d3": "s1d3_filtered_feature_bc_matrix.h5",
+}
+adatas = {}
+
+for sample_id, filename in samples.items():
+    path = EXAMPLE_DATA.fetch(filename)
+    sample_adata = sc.read_10x_h5(path)
+    sample_adata.var_names_make_unique()
+    adatas[sample_id] = sample_adata
+
+adata = ad.concat(adatas, label="sample")
+adata.obs_names_make_unique()
+print(adata.obs["sample"].value_counts())
+adata
